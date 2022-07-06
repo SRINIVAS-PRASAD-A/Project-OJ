6cdf00c3487d5a6c4be4257d13f2ec0e72069f23
Srinivas Prasad <annaldasasprasad2@gmail.com>
2022-07-06 23:25:26 +0530

    added views

--- judge/urls.py
+++ judge/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'judge'
 urlpatterns = [
-   
+    path('',views.problems,name='problems'),
+    path('problem/<int:problem_id/',views.problemDetail,name='problem_detail'),
+    path('problem/<int:problem_id/submit/',views.submitProblem,name='submit'),
+    path('leaderboard',views.leaderboard,name='leaderboard'),
 ]
--- judge/views.py
+++ judge/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.shortcuts import render,get_object_or_404
 
+
+from .models import Problem,Solution
+import os,filecmp
 # Create your views here.
+
+def problems(request):
+    problems_list = Problem.objects.all()
+    context = {'problems_list':problems_list}
+    return render(request,'judge/index.html',context)
+
+def problemDetail(request,problem_id):
+    problem = get_object_or_404(Problem,pk=problem_id)
+    return render(request,'judge/detail.html',{'problem':problem})
+
+def submitProblem(request,problem_id):
+    
+    pass
+
+def leaderboard(request):
+    solutions = Solution.objects.all()
+    return render(request,'judge/leaderboard.html',{'solutions':solutions})
